149210ac31addccede09abfeec0f1772cc691c94
DarkarPacheco <jaunchoenelcarro@gmail.com>
2023-06-16 14:29:38 -0600

    se agrega edad

---
+++ Unidad1/12.Calificacionesbool.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.mavenproject6;
+
+
+public class Mavenproject6 {
+
+    public static void main(String[] args) {
+        int edadJuan=22;
+        int edadPedro=20;
+        boolean bandera1;
+        bandera1=(edadJuan>edadPedro);
+        System.out.println("Resultado de la bandera" + bandera1);
+        int edadLupe=35;
+        boolean bandera2;
+        boolean bandera3;
+        bandera2=(edadLupe<edadPedro);
+        bandera3=(bandera1&&bandera2);
+        System.out.println("Resultado de bandera3:" + bandera3);
+        
+    }
+    
+}
