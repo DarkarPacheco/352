adea8503cece021c5bfbe9e5f79630dadbd18f20
DarkarPacheco <jaunchoenelcarro@gmail.com>
2023-06-29 10:03:02 -0600

    se agrega factorial

---
+++ Unidad2/2-7FORFACTORIAL.java
@@ -0,0 +1,18 @@
+package com.mycompany.forfactorial;
+
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        double factorial;
+        int num; 
+        System.out.println("Introduce el numero: ");
+        num=entrada.nextInt();
+        factorial=1;
+        for(int i = num; i>0; i--){
+            factorial=factorial*1;
+        }
+        System.out.println("El factorial de " + num + "es: " + factorial);
+    }
+}
